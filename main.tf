22d70f7d42583e8cbf247700b5657152aeee4065
Marcus Apolinario <marcus.apolinario@processor.com.br>
2022-01-11 01:07:53 -0300

    Feat: Create Storage Account

--- main.tf
+++ main.tf
@@ -16,3 +16,20 @@ resource "azurerm_subnet" "this" {
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.10.0.0/24"]
 }
+
+resource "azurerm_storage_account" "this" {
+  name                = "storageaccount"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+
+  account_kind             = "StorageV2"
+  account_tier             = "Premium"
+  account_replication_type = "GRS"
+  access_tier              = "Hot"
+
+  tags = {
+    Terraform   = true
+    Environment = "Staging"
+    Owner       = "marcusv.sa@hotmail.com"
+  }
+}
